9b404e8ea57ab64211830211ab0dbba26c5b57ea
Robin Cosbey <robin.cosbey@pnnl.gov>
2023-12-09 12:58:15 -0800

    url scraping

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     description='advent of code',
     author='robin cosbey',
     packages=['aoc'],
-    install_requires=['pandas==2.1.3', 'advent-of-code-data']
+    install_requires=['pandas==2.1.3', 'advent-of-code-data', 'beautifulsoup4==4.12.2']
 )
